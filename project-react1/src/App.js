d5d8b82f2c47fb884fb935f81d325c01121ca0e8
Daniel Lima <111375723+danieldelimajj@users.noreply.github.com>
2023-08-20 12:25:46 -0300

    Update App.js

--- project-react1/src/App.js
+++ project-react1/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import People from './assets/people.svg'
 import Arrow from './assets/arrow.svg'
@@ -16,22 +16,22 @@ import {
 } from './styles';
 
 function App() {
-  const [ users, setUsers ] = useState([]);
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
-   
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
 
-function addNewUser(){
-  setUsers([...users,{id: Math.random(), name, age}])
-}; //Adiciona mais de um usuário ao preencher o formulário (spread operator)
 
-function changeInputName(event) {
-  setName(event.target.value)
-}
 
-function changeInputAge(event) {
-  setAge(event.target.value)
-}
+  function addNewUser() {
+
+    setUsers([...users, { id: Math.random(), name: inputName.current.value, age: inputAge.current.value }])
+  }; //Adiciona mais de um usuário ao preencher o formulário (spread operator)
+
+  function deleteUser(userId) {
+    const newUsers = users.filter((user) => user.id !== userId); //Cria uma função para deletar um usuário ao clicar na lixeira;
+
+    setUsers(newUsers);
+  }
 
   return (
     <Container>
@@ -40,23 +40,25 @@ function changeInputAge(event) {
         <H1>Olá!</H1>
 
         <InputLabel>Nome</InputLabel>
-        <Input onChange={changeInputName} placeholder='Nome' />
+        <Input ref={inputName} placeholder='Nome' />
 
         <InputLabel>Idade</InputLabel>
-        <Input onChange={changeInputAge} placeholder='Idade' />
+        <Input ref={inputAge} placeholder='Idade' />
 
-        <Button onClick={addNewUser}>Cadastrar <img alt='seta' src={Arrow}></img></Button>
+        <Button onClick={() => addNewUser(User.id)}>Cadastrar <img alt='seta' src={Arrow}></img></Button>
 
 
         <ul>
           {users.map((user) => (
             <User key={user.id}>
-              <p>{user.name}</p>  
+              <p>{user.name}</p>
               <p>{user.age}</p>
-              <button><img src={Trash} alt='Lata de lixo'/></button>
-              </ User>            
 
-          )) }
+              <button onClick={() => deleteUser(user.id)}>
+                <img src={Trash} alt='Lata de lixo' /></button>
+            </ User>
+
+          ))}
         </ul>
       </ContainerItens>
     </Container>
